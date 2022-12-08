feffc646a6cfcc19f91ad1b4f8db2ed5c911db7c
Julian Benchimol <jbenchimol1999@gmail.com>
2022-12-08 10:59:25 -0500

    schema tables created

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,26 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department {
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
-};
+CREATE TABLE department (
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(30)
+);
 
-CREATE TABLE role {
+CREATE TABLE role (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(6, 2) NOT NULL,
-    department_id INT NOT NULL
-}
+    name VARCHAR(30) NOT NULL,
+    salary DECIMAL (6,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+);
 
-CREATE TABLE employee {
+CREATE TABLE employee(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
-}
+    manager_id INT NOT NULL,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (id, name)
+VALUES ("Engineering"),("Finannce"),("Legal"),("Sales");
+
+INSERT INTO role(id, title, salary, department_id)
+VALUES
